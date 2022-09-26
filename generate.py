0924d2673d9155ccbc5680b40aae6edad30f6004
Bnyro <bnyro@tutanota.com>
2022-09-26 11:33:57 +0200

    add doc title config

--- generate.py
+++ generate.py
@@ -43,6 +43,12 @@ def get_file_content(file_path) -> dict:
     return info
 
 def replace_by_key(text: str, dict: dict) -> str:
+    """
+    Replace the keys of a dict with their values inside a text
+    :param text: The base text to modify
+    :param dict: The dict object containing the fields to replace
+    :return The modified text as string
+    """
     for key in dict:
         text = text.replace("{{" + key + "}}", str(dict[key]))
     return text
