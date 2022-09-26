8bb1652c464fce60109ed16f9bbde6f73d7f0eb3
Bnyro <bnyro@tutanota.com>
2022-09-26 13:15:46 +0200

    add printing option

--- static/app.js
+++ static/app.js
@@ -19,3 +19,13 @@ if (selectedTheme) {
     $('html').classList.add(selectedTheme);
     $('#theme-select').value = selectedTheme;
 }
+
+$('#print').addEventListener('click', () => {
+    var content = $('section').innerHTML;
+    var WinPrint = window.open('', '_blank', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+    WinPrint.document.write(content);
+    WinPrint.document.close();
+    WinPrint.focus();
+    WinPrint.print();
+    WinPrint.close();
+})
